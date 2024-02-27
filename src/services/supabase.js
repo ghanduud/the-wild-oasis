3a7320d1263de5b0bf480158036dbafc5a283a41
Ghanduud <omar.ghandy780@gmail.com>
2024-02-27 20:09:11 +0200

    adding cabin and setting features

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+export const supabaseUrl = 'https://reashixnwoknxrorazfz.supabase.co';
 
-const supabaseUrl = 'https://reashixnwoknxrorazfz.supabase.co';
 const supabaseKey =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlYXNoaXhud29rbnhyb3JhemZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3ODA3MDUsImV4cCI6MjAyNDM1NjcwNX0.YFHYlOqG-1IBAiXWgL2x2-jWj5Z-RnyeeHht9du7BWc';
 const supabase = createClient(supabaseUrl, supabaseKey);
