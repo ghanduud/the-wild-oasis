3baa1de27c4c8cdef747178ce04f9e9c0f345bfc
Ghanduud <omar.ghandy780@gmail.com>
2024-03-01 05:16:32 +0200

    setting up advanced patterns

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://reashixnwoknxrorazfz.supabase.co';
+import { createClient } from "@supabase/supabase-js";
 
+export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
 const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlYXNoaXhud29rbnhyb3JhemZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3ODA3MDUsImV4cCI6MjAyNDM1NjcwNX0.YFHYlOqG-1IBAiXWgL2x2-jWj5Z-RnyeeHht9du7BWc';
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
